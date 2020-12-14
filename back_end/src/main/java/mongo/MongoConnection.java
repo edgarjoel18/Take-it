e5f256a325b6dbf369a00e9d47b2f788edcb1e04
rmsevilla <rsevill1@mail.sfsu.edu>
2020-12-13 18:29:05 -0800

    Fixed the greyed out variables

--- back_end/src/main/java/mongo/MongoConnection.java
+++ back_end/src/main/java/mongo/MongoConnection.java
@@ -24,12 +24,13 @@ public class MongoConnection {
                 fromProviders(provider));
         mongoClient = new MongoClient("localhost",27017);
         database = mongoClient.getDatabase("CSC413");
+        database = database.withCodecRegistry(pojoCodecRegistry);
     }
 
 
     /* Get the Collections from database */
     public MongoCollection getCollection(String name, Class<ListingDao> listingDaoClass){
-        return database.getCollection(name);
+        return database.getCollection(name, listingDaoClass);
     }
 
 }
